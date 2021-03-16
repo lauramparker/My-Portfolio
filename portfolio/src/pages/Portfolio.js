24bc177ac16a778413d8e4e8105a7b8ad18c7918
Laura Parker <lauramparker78@gmail.com>
2021-03-16 10:39:02 -0400

    Fixing syntax

--- portfolio/src/pages/Portfolio.js
+++ portfolio/src/pages/Portfolio.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import Container from "../components/container";
 import Header from "../components/header";
 import Footer from "../components/footer";
 import Row from "../components/Row";
 import Col from "../components/Col";
 import ProjectCard from "../components/projectCard";
-import projects from "./projects.json";
+import projects from "../projects.json";
 
 function Portfolio() {
     return (
