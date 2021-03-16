5dc8da3f450a6a7bae7ab6b2934a8f6ff13f3857
Laura Parker <lauramparker78@gmail.com>
2021-03-16 10:52:13 -0400

    Updating bootstrap link before App import in index

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/header/index";
 import Footer from "./components/footer/index";
 import About from "./pages/About";
@@ -10,14 +10,16 @@ import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <div>
-      <Header></Header>
-      <Route exact path="/" component={About} />
-      <Route exact path="/about" component={About} />
-      <Route exact path="/portfolio" component={Portfolio} />
-      <Route exact path="/contact" component={Contact} />
-      <Footer></Footer>
-    </div>
+    <Router>
+      <div>
+        <Header></Header>
+        <Route exact path="/" component={About} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/contact" component={Contact} />
+        <Footer></Footer>
+      </div>
+    </Router>
   );
 }
 
