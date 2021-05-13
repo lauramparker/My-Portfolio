c8baca6b6d9aa1e492cf205f3c4a4accd961c3bc
Laura Parker <lauramparker78@gmail.com>
2021-05-13 08:45:01 -0400

    Styling project card info

--- src/components/projectCard/index.js
+++ src/components/projectCard/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Card, ListGroup } from "react-bootstrap";
 
 function ProjectCard(props) {
     return (
@@ -8,7 +8,12 @@ function ProjectCard(props) {
             <Card.Img className='img' variant="top" src={props.image}/>
             <Card.Body>
                 <Card.Text>{props.description}</Card.Text>
-                <Card.Text>Technologies Used: {props.technologies}</Card.Text>
+                <ListGroup>Technologies Used: {(props.technologies).map(item=> {
+                    return(
+                        <li key={item}>{item}</li>
+                    )}
+                )}
+                </ListGroup>
                 <Card.Link href={props.repo} style={{position:'absolute', bottom: '10px'}}><p><u>GitHub Repository</u></p></Card.Link>
                 <Card.Link href={props.deploy} style={{position:'absolute', right: '30px', bottom: '10px'}}><p><u>Deployed App</u></p></Card.Link>
             </Card.Body>
--- src/pages/About.js
+++ src/pages/About.js
@@ -14,27 +14,32 @@ function About() {
                         <div>
                             <img src="https://lauramparker.github.io/My-Portfolio/images/LMPheadshot.png" id="headshot" alt="Laura M. Parker headshot" ></img>
                         </div>
-                        <div className='paragraph' style={{ width: '70%' }}>
+                        <div className='paragraph' style={{ width: '80%' }}>
                             <p> I am a mid-career professional based in Columbus, OH with broad experience in both large and small organizations.  I have deep experience as a leader and proven project manager in the learning and develpment industry, working across multiple disciplines (IT, sales & marketing, research, and product management).
                             Eight years of my professional experience were spent working with media and publishing corporations, with whom I held roles as a consultant for digital products, product manager, and VP of Solution Architects.
                             In addition, I guided relationships and product alignment between content develop teams and third-party software companies.
                             In the years that followed, I worked as a consultant for both product development and implementation within the learning and development industry, leveraging my experience with digital learning solutions.
                             This included short-term contracts with market research firms, and longer engagements with start-up projects and organizations.</p>
-                            <p> More recently, I have invested in my own professional development and career transition.  I have acquired new skills in fullstack web development and marketing analytics, in addition to launching a small business.
+                            <p> More recently, I have invested in my own professional development and career transition.  I have acquired new skills in fullstack web development and marketing analytics.
                             At this stage of life and career, I am interested in opportunities to grow my experience as a full-stack developer, with hopes of leading an innovative product development team in the future.
                             Outside of work, I enjoy playing with my two young children, gardening, volunteering, and exploring our beautiful local and National Parks.</p>
 
                         </div>
-                        <Card style={{ width: '35%' }}>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col>
+                        <Card style={{ width: '75%' }}>
                             <Card.Header>
                                 <small><strong>Please Contact Me:</strong></small>
                             </Card.Header>
-                            <ListGroup>
+                            <ListGroup variant='flush'>
                                 <ListGroupItem><Link><small>lauramparker78@gmail.com</small></Link></ListGroupItem>
                                 <ListGroupItem><small>(m) 386-747-8362</small></ListGroupItem>
                                 <ListGroupItem><Link><small><a href>https://www.linkedin.com/in/laura-parker-5a3bbb198/</a></small></ Link></ListGroupItem>
                                 <ListGroupItem><Link><small><a href>https://github.com/lauramparker</a></small></Link></ListGroupItem>
-                                <ListGroupItem variant='light'><small><strong>Download My Resume</strong></small>
+                                <ListGroupItem variant='light'><small><strong>Download My Resume</strong></small><br></br>
                                     <a href="./images/LMP-Resume.pdf" download>
                                         <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-down-circle-fill" fill="#e98074" xmlns="http://www.w3.org/2000/svg">
                                             <path fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z" />
@@ -44,6 +49,31 @@ function About() {
                             </ListGroup>
                         </Card>
                     </Col>
+
+                    <Col>
+                        <Card style={{ width: '75%' }}>
+                            <Card.Header>
+                                <small><strong>Skills:</strong></small>
+                            </Card.Header>
+                            <ListGroup variant='flush'>
+                                <ListGroupItem><small>Technical:</small>
+                                    <li>Javascript</li>
+                                    <li>Node/Express</li>
+                                    <li>MongoDB, SQL</li>
+                                    <li>React, Angular, JQuery</li>
+                                    <li>HTML/CSS, Bootstrap, Material-UI, UIKit </li>
+                                    </ListGroupItem>
+                                <ListGroupItem><small>Operational:</small>
+                                    <li>Project Management</li>
+                                    <li>Talent Management</li>
+                                    <li>Messaging & Communication</li>
+                                    <li>Budget Planning</li>
+                                    </ListGroupItem>
+                            </ListGroup>
+                        </Card>
+                    </Col>
+
+
                 </Row>
             </Container>
             <Footer />
