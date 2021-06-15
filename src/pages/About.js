a3cb0a9e0378ab12f2ce3f98c93f2b7e934013f3
Laura Parker <lauramparker78@gmail.com>
2021-06-15 14:39:22 -0400

    Fixing resume link

--- src/pages/About.js
+++ src/pages/About.js
@@ -40,7 +40,7 @@ function About() {
                                 <ListGroupItem><Link><small><a href>https://www.linkedin.com/in/laura-parker-5a3bbb198/</a></small></ Link></ListGroupItem>
                                 <ListGroupItem><Link><small><a href>https://github.com/lauramparker</a></small></Link></ListGroupItem>
                                 <ListGroupItem variant='light'><small><strong>Download My Resume</strong></small><br></br>
-                                    <a href="./images/LMP-Resume.pdf" download>
+                                    <a href="https://raw.githubusercontent.com/lauramparker/My-Portfolio/main/public/images/LMP_resume.png" download>
                                         <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-down-circle-fill" fill="#e98074" xmlns="http://www.w3.org/2000/svg">
                                             <path fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z" />
                                         </svg>
